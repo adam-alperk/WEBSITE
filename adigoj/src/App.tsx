4d4f1c1ae9d05c0090ecb44ec54725fb0e366194
Aditya Gojamgunde <adam.alperk@gmail.com>
2024-02-03 16:06:16 +0530

    Fixed home page import in App.tsx

--- adigoj/src/App.tsx
+++ adigoj/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home"; // Adjust the import path according to your file structure
+import Home from "./pages/home.tsx"; // Adjust the import path according to your file structure
 
 const App: React.FC = () => {
   return (
