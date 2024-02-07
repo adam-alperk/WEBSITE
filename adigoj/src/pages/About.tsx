393ea4a4473e8af152508348f6e1387175353257
adam.alperk@gmail.com <adityagojamgunde@mac-ws.local>
2024-02-07 23:29:15 +0530

    Small Changes.

--- adigoj/src/pages/About.tsx
+++ adigoj/src/pages/About.tsx
@@ -1,11 +1,27 @@
-import React from "react";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom"; // Import Link
 import "../App.css";
 
 function About() {
   return (
-    <div className="bg">
-      <h1>About</h1>
-    </div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className="container">
+        <div className="navigation">
+          <Link to="/" className="navButton">
+            Home
+          </Link>
+          <Link to="/about" className="navButton">
+            About
+          </Link>
+        </div>
+        <h1>About</h1>
+      </div>
+    </motion.div>
   );
 }
 
