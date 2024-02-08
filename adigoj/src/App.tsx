66b9b9a79b2bcfa0f38762c31a35fbe788c70335
adam.alperk@gmail.com <adityagojamgunde@mac-ws.local>
2024-02-08 20:17:45 +0530

    Remove redundant contact page. Contact information on home page.

--- adigoj/src/App.tsx
+++ adigoj/src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Background from "./assets/Background.tsx";
 import Home from "./pages/Home.tsx";
 import Work from "./pages/Work.tsx";
-import Contact from "./pages/Contact.tsx";
 import About from "./pages/About.tsx";
 import "./App.css";
 
@@ -15,7 +14,6 @@ function App() {
         <Routes key={location.pathname} location={location}>
           <Route index element={<Home />} />
           <Route path="work" element={<Work />} />
-          <Route path="contact" element={<Contact />} />
           <Route path="about" element={<About />} />
         </Routes>
       </main>
